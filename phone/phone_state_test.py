bfd69973968f5b66b46d769841db4df007d92d78
Andreas Eckleder <andreas@eckleder.de>
2018-02-17 15:46:45 +0100

    Using mock framework.

--- phone/phone_state_test.py
+++ phone/phone_state_test.py
@@ -1,4 +1,5 @@
 import logging
+import mock
 import phone_state
 import unittest
 
@@ -7,50 +8,36 @@ class TestPhoneState(unittest.TestCase):
     logging.basicConfig(level=logging.INFO)
 
   def test_BasicStateTest(self):
+    m = mock.Mock()
     state_machine = phone_state.PhoneState(
       0, {('a', 0): 1, ('b', 1): 0, ('d', 0): 2},
-      {(0, 1): [self.callback01],
-       (1, 0): [self.callback10]})
+      {(0, 1): [m.callback01],
+       (1, 0): [m.callback10]})
     
-    self.callback01_ = False
-    self.callback10_ = False
     state_machine.ProcessInput('a')
-    self.assertEqual(True, self.callback01_)
-    self.assertEqual(False, self.callback10_)
+    m.callback01.assert_called_once_with(0, 1)
+    m.callback10.assert_not_called()
     self.assertEqual(1, state_machine.GetCurrentState())
 
-    self.callback01_ = False
-    self.callback10_ = False
+    m.reset_mock()
     state_machine.ProcessInput('b')
-    self.assertEqual(False, self.callback01_)
-    self.assertEqual(True, self.callback10_)
+    m.callback01.assert_not_called()
+    m.callback10.assert_called_once_with(1, 0)
     self.assertEqual(0, state_machine.GetCurrentState())
 
-    self.callback01_ = False
-    self.callback10_ = False
+    m.reset_mock()
     # Unknown input symbol, should do nothing.
     state_machine.ProcessInput('c')
-    self.assertEqual(False, self.callback01_)
-    self.assertEqual(False, self.callback10_)
+    m.callback01.assert_not_called()
+    m.callback10.assert_not_called()
     self.assertEqual(0, state_machine.GetCurrentState())
-    
-    self.callback01_ = False
-    self.callback10_ = False
+
+    m.reset_mock()
     # State transition without callbacks.
     state_machine.ProcessInput('d')
-    self.assertEqual(False, self.callback01_)
-    self.assertEqual(False, self.callback10_)
+    m.callback01.assert_not_called()
+    m.callback10.assert_not_called()
     self.assertEqual(2, state_machine.GetCurrentState())
-    
-  def callback01(self, previous, next):
-    self.assertEqual(0, previous)
-    self.assertEqual(1, next)
-    self.callback01_ = True
-
-  def callback10(self, previous, next):
-    self.assertEqual(1, previous)
-    self.assertEqual(0, next)
-    self.callback10_ = True
 
 if __name__ == '__main__':
   unittest.main()
